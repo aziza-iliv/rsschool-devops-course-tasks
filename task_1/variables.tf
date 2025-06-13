2352a930cd45f8edfeb4ab160d75e460788c3be3
aziza <ilieva.aziza02@gmail.com>
2025-06-13 18:33:39 +0500

    Add Terraform and GitHub Actions for OIDC deployment

--- task_1/variables.tf
+++ task_1/variables.tf
@@ -10,12 +10,6 @@ variable "bucket_name" {
   default     = "aziza-tfstate-20250607"
 }
 
-variable "dynamodb_table" {
-  description = "DynamoDB table for state locking"
-  type        = string
-  default     = "rs_task_list"
-}
-
 variable "environment" {
   description = "Deployment environment"
   type        = string

dfb69fff6f3a47985a34cbb857fa4cb739dd91f3
aziza <ilieva.aziza02@gmail.com>
2025-06-28 22:45:18 +0500

    Removed .terraform from the index and updated working files

--- task_3/Terraform_solution/main.tf
+++ task_3/Terraform_solution/main.tf
@@ -89,3 +89,11 @@ resource "aws_instance" "k3s_master" {
 resource "aws_instance" "k3s_worker" {
   ami                    = var.ami
   instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "K3s-Worker"
+  }
+}
